bfc9f637075d386faedd2f15ddc0064090d107ed
Mrázek Adam <mrazek.adam@ifortuna.cz>
2018-12-23 13:52:59 +0100

    Failing test example

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,11 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+/* eslint-disable no-undef */
+import * as testPkg from '../../tests/util'
+import sinon from 'sinon'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
+  it('Should pass', () => {
+    sinon.stub(testPkg, 'bar').returns(2)
+    testPkg.bar() // ?
+    expect(testPkg.bar()).to.be.equal(2)
   })
 })
---
+++ wallaby.js
@@ -0,0 +1,59 @@
+const webpackConfig = require('./node_modules/@vue/cli-service/webpack.config.js')
+const wallabyWebpack = require('wallaby-webpack')
+
+module.exports = function (wallaby) {
+  webpackConfig.resolve.alias = { '@': require('path').join(wallaby.projectCacheDir, 'src') }
+  webpackConfig.plugins = webpackConfig.plugins
+    .filter(p =>
+      p.constructor.name !== 'ProgressPlugin' &&
+      p.constructor.name !== 'HotModuleReplacementPlugin')
+
+  const wallabyPostprocessor = wallabyWebpack(webpackConfig)
+
+  return {
+    files: [
+      { pattern: 'node_modules/vue/dist/vue.js', instrument: false },
+      { pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false },
+      { pattern: 'node_modules/chai/chai.js', instrument: false },
+      { pattern: 'node_modules/sinon/pkg/sinon.js', instrument: false },
+      { pattern: '.postcssrc.js', instrument: false, load: false },
+      { pattern: '.eslintrc.js', instrument: false, load: false },
+      { pattern: 'src/**/*.*', load: false },
+      { pattern: 'tests/**/*.js', load: false },
+      { pattern: 'tests/**/*.spec.js', ignore: true },
+      { pattern: 'tests/**/.*', ignore: true }
+    ],
+
+    env: {
+      type: 'browser',
+      kind: 'chrome'
+    },
+
+    tests: [
+      { pattern: 'tests/**/*.spec.js', load: false }
+    ],
+
+    postprocessor: wallabyPostprocessor,
+
+    testFramework: 'mocha',
+
+    setup: function () {
+      // eslint-disable-next-line
+      Vue.config.errorHandler = function (err) {
+        throw err
+      }
+      window.expect = chai.expect
+      window.__moduleBundler.loadTests()
+    },
+
+    hints: {
+      ignoreCoverage: /ignore coverage/
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+      '**/*.vue': require('wallaby-vue-compiler')(wallaby.compilers.babel({}))
+    },
+    debug: true
+  }
+}
